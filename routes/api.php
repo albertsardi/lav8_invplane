7a7e2c56c0649022dc484b89fbc2d0186bfe3df2
albert <albertsardi@gmail.com>
2024-08-02 01:42:57 -0700

    first init ->import from laravel 7

---
+++ routes/api.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Post;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::prefix('api')->group(function() {
+    
+    // general create
+    Route::post('customer/create', 'ApiController@createdata');
+    Route::post('customer/update/{id}', 'ApiController@updatedata');
+    
+    // form master load
+    Route::get('product/{id?}', 'AjaxController@ajax_getProduct');
+    Route::get('customer/{id?}', 'AjaxController@ajax_getCustomer');
+    Route::get('supplier/{id?}', 'AjaxController@ajax_getSupplier');
+    Route::get('coa/{id?}', 'AjaxController@ajax_getCoa');
+
+    // from common
+    Route::get('common/{id?}', 'ApiController@getcommon');
+    // from trans load
+    Route::get('{jr}/{id?}', 'ApiController@getdata');
+    // Route::get('{jr}/{id?}', 'ApiController@getdata');
+
+    
+
+     // select2 / combobox
+     //Route::get('select/{jr}', 'MainController@selectData');
+
+    // data save
+    Route::prefix('datasave')->group(function() {
+        Route::post('/', 'TransController@datasave_PI');
+        Route::post('product', 'MasterController@datasave_product');
+        Route::post('PI', 'TransController@datasave_PI');
+    });
+
+    Route::post('transsave', 'TransController@transsave');
+
+    
+
+});
+
+// report /pdf
+Route::get('report/pdf', 'ReportController@testpdf');
+
+Route::prefix('ajax')->group(function() {
+    //chart in dashboard
+    Route::get('makechart/{id}', 'AppController@makechart');
+
+    // data save
+    Route::any('datasave', 'AjaxController@datasave');
+});
+
+//API route tuk crud
+//include('crud.php');
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+//use App\Http\Controllers\AppController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//app
+Route::any('/', 'App\Http\Controllers\AppController@dashboard');
+Route::get('/dashboard', 'App\Http\Controllers\AppController@dashboard');
+
+/* 
+    router tuk coba2
+*/
+Route::prefix('test')->group(function($e) {
+    // form master load
+    Route::get('reportPDF', 'TestController@reportPDF');
+    Route::get('koolreport/chart', 'TestController@koolreportchart');
+    Route::get('koolreport/chart/pdf', 'TestController@koolreportchart_pdf');
 });
